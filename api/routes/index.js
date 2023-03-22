7a783900bf249a70dc7c1e4941ff0e4be9894b06
Jorge Mendoza <mencor.corp@gmail.com>
2023-03-22 17:22:22 -0600

    deploy sin artiulos

--- api/routes/index.js
+++ api/routes/index.js
@@ -11,12 +11,12 @@ function routerApp(app) {
   const routerV1 = express.Router();
   const routerV2 = express.Router();
 
-  app.use('/api/v1', routerV1);
+  //app.use('/api/v1', routerV1);
   app.use('/api/v2', routerV2);
 
   // ruteos de v1
-  routerV1.use('/personas', personasRouter);
-  routerV1.use('/articulos', articulosRouter);
+  //routerV1.use('/personas', personasRouter);
+  //routerV1.use('/articulos', articulosRouter);
 
   // ruteos de v2
   routerV2.use('/personas', personasRouterV2);
